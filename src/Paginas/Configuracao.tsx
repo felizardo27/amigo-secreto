64bf502fc1df629f1a25830a68ff9689e29bb677
João Pedro Felizardo <jp.felizardo27@gmail.com>
2022-09-20 21:23:12 -0300

    Adicioanndo teste de configurações com snapshot

---
+++ src/Paginas/Configuracao.tsx
@@ -0,0 +1,17 @@
+import Formulario from "../Components/Formulario";
+import ListaParticipantes from "../Components/ListaParticipantes";
+import Rodape from "../Components/Rodape";
+
+const Configuracao = () => {
+
+    return (
+        <>
+            <Formulario />
+            <ListaParticipantes />
+            <Rodape />
+        </>
+    );
+
+};
+
+export default Configuracao;
