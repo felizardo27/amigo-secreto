89b6a7c0324eead8dc46140ab9d454c7b5c6da0b
João Pedro Felizardo <jp.felizardo27@gmail.com>
2022-09-28 07:13:07 -0300

    Adicionando Hook Usesorteador

---
+++ src/state/hook/useSorteador.ts
@@ -0,0 +1,27 @@
+import shuffle from "just-shuffle";
+import { stringify } from "querystring";
+import { useSetRecoilState } from "recoil";
+import { resultadoAmigoSecreto } from "../atom";
+import { useListaParticipantes } from "./useListaParticipantes";
+
+export const useSorteador = () => {
+
+    const participantes = useListaParticipantes()
+    const setResultado = useSetRecoilState(resultadoAmigoSecreto);
+
+    return () => {
+        
+        const totalDeParticipantes = participantes.length;    
+        const embaralhado = shuffle(participantes);
+        const resultado = new Map<string, string>();
+
+        for (let index = 0; index < totalDeParticipantes; index++) {
+        
+            const indiceDoAmigo = index === (totalDeParticipantes - 1) ? 0 : index + 1;
+            resultado.set(embaralhado[index], embaralhado[indiceDoAmigo]);
+        }
+
+        setResultado(resultado);
+    };
+
+};
