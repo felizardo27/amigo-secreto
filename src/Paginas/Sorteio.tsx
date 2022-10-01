9f3be58e6118e9eea34bb77e9b164aad5e3bcda7
João Pedro Felizardo <jp.felizardo27@gmail.com>
2022-10-01 14:29:19 -0300

    finalizando estilos sorteio

--- src/Paginas/Sorteio.tsx
+++ src/Paginas/Sorteio.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
+import Card from "../Components/Card";
 import { useListaParticipantes } from "../state/hook/useListaParticipantes";
 import { useResultadoDoSorteio } from "../state/hook/useResultadoDoSorteio";
-import styles from './sorteio.module.scss';
+import styles from './Sorteio.module.scss';
 
 
 const Sorteio = () => {
@@ -21,24 +22,30 @@ const Sorteio = () => {
     }
 
     return (
-        <section>
-            <form onSubmit={sortear}>
-                <select 
-                    required
-                    name="participanteDaVez" 
-                    id="ParticipanteDaVez"
-                    placeholder="Selecione o seu nome"
-                    value={participanteDaVez}
-                    onChange={evento => setParticipanteDaVez(evento.target.value)}
-                >
-                    {participantes.map(participante => 
-                        <option key={participante}>{participante}</option>
-                        )}
-                </select>
-                <button>Sortear</button>
-                {amigoSecreto && <p role="alert">{amigoSecreto}</p>}
-            </form>
-        </section>
+        <Card>
+            <section className={styles.sorteio}>
+            <h2>Quem vai tirar o papelzinho?</h2>
+                <form onSubmit={sortear}>
+                    <select
+                        required
+                        name="participanteDaVez"
+                        id="ParticipanteDaVez"
+                        placeholder="Selecione o seu nome"
+                        value={participanteDaVez}
+                        onChange={evento => setParticipanteDaVez(evento.target.value)}
+                    >
+                        {participantes.map(participante =>
+                            <option key={participante}>{participante}</option>
+                            )}
+                    </select>
+                    <button className={styles.botaoSortear}>Sortear</button>
+                </form>
+                    {amigoSecreto && <p className={styles.resultado} role="alert">{amigoSecreto}</p>}
+                    <footer className={styles.sorteio}>
+                            <img src="/imagens/aviao.png" className="aviao"  alt="Um desenho de um avião de papel" />
+                    </footer>
+            </section>
+        </Card>
     );
 };
 
